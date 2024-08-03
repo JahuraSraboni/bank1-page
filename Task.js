c38394305a03bfc62b88fe9745f051cf74ac4b51
JahuraSraboni <159037706+JahuraSraboni@users.noreply.github.com>
2024-08-03 20:23:52 +0600

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,23 @@
+
+function jon(){    
+     document.write("Subscribed");
+    }
+
+ function jon1(){
+    alert("Thanks");
+ }
+
+ function jon2(){
+    alert("Welcome");
+ }
+
+ function jon3(){
+    alert(" Signing");
+ }
+ function jon4(){
+    alert(" Signing");
+ }
+
+ function jon5(){    
+    document.write("learning");
+   }
